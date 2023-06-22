f7a5b190228788947ac90348c29c55300e38b9df
wonjineeds <jineeds7@gmail.com>
2023-06-22 15:26:49 +0900

    Update views.py

--- backend/ohsori/views/login_views.py
+++ backend/ohsori/views/login_views.py
@@ -21,23 +21,6 @@ from django.contrib.auth import login
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from knox.views import LoginView as KnoxLoginView
 
-# class LoginAPI(KnoxLoginView):
-#     permission_classes = (permissions.AllowAny,)
-
-#     def post(self, request, format=None):
-#         serializer = AuthTokenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.validated_data['user']
-#         login(request, user)
-
-#         # Create token and save it in CustomToken
-#         _, token = AuthToken.objects.create(user)
-
-#         # Delete existing token if exists
-#         CustomToken.objects.filter(user=user).delete()
-#         CustomToken.objects.create(user=user, token=token)
-
-#         return super(LoginAPI, self).post(request, format=None)
 
 # 회원가입할때의 토큰을 저장하니깐 로그아웃한 후 다시 로그인했을때 db에 저장되어있는 토큰으로는 정보조회, 로그아웃을 못한다
 # 그래서 로그인할때마다 토큰을 저장하고 다시 로그인하면 다시 새로운 토큰으로 저장되게 하기
