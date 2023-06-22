450d85509c08a4045c8c47c2c5ef6848bdfac800
en7hus1asm <suzie7185@naver.com>
2023-06-22 09:48:47 +0900

    faq구현

--- frontend/src/pages/items/Basket.js
+++ frontend/src/pages/items/Basket.js
@@ -1,4 +1,5 @@
 import React from "react";
+import styles from "./Basket.module.css";
 
 const User = ({userData}) => {
     return (
@@ -9,9 +10,33 @@ const User = ({userData}) => {
     )
 }
 
+function BasketBoxes() {
+    return (
+        <div className={styles.bk_body}>
+        <div className={styles.bklist}>
+            <div className={styles.bklist_img}>이미지가들어갑니당</div>
+            <div className={styles.bklist_com}>
+                <div className={styles.bklist_name}>영덕 대게</div>
+                <div className={styles.bklist_txt}>판매자 : ㅇㅇ스토어</div>
+                <div className={styles.bklist_price}>58000원</div>
+            </div>
+            <div className={styles.bklist_btbox}>
+                <button className={styles.bklist_bt1}>
+                    <div className={styles.bklist_btfont}>더 보기</div>
+                </button>
+                <button className={styles.bklist_bt2}>
+                    <div className={styles.bklist_btfont}>삭제</div>
+                </button>
+            </div>
+        </div>
+    </div>
+    );
+}
+
 const Basket = () => {
     const users = [
-        {email: 'chrisjyh@naver.com', name: "주은호"},
+        // 받아올 정보 db모음
+        {zzim_name: 'chrisjyh@naver.com', zzim_img: "123.png" ,zzim_date: "주은호"},
         {email: 'chrisjyh@naver.com', name: "주은호"},
         {email: 'chrisjyh@naver.com', name: "주은호"},
         {email: 'chrisjyh@naver.com', name: "주은호"},
@@ -20,11 +45,13 @@ const Basket = () => {
 
     return (
         <div>
-            <ul>
-                {users.map(user => <User userData={user} />)}
-            </ul>
+            <div className={styles.bklist_main}>(v)  찜한 상품입니다.</div>
+            <BasketBoxes/>
+            <BasketBoxes/>
+            <BasketBoxes/>         
         </div>
-    )
-}
 
+
+    );
+};
 export { Basket };
--- frontend/src/pages/main/Home.js
+++ frontend/src/pages/main/Home.js
@@ -1,15 +1,103 @@
 import React, { useState } from "react";
 import {send} from "../../components";
+import styles from "./Home.module.css";
 
 const Home = () => {
+    const [currentPage, setCurrentPage] = useState('first');
+
+    const goToSecondPage = async () => {
+        await new Promise(resolve => setTimeout(resolve, 30));
+        setCurrentPage('second');
+    };
+
+    return(
+        <div>
+        {currentPage === 'first' && <FirstPage goToSecondPage={goToSecondPage} />}
+        {currentPage === 'second' && <SecondPage />}
+      </div>
+    );
+};
+
+const FirstPage = ({goToSecondPage}) => {
     const data = ['hi','hi','hi','hi','hi']
-        return (
-        <>
-            <h1 className="title">home 화면임</h1>
-            <button onClick={() => send('first_fun', "data",'back')}> 보내기 버튼!!!!!! </button> 
-        </>
-    
+    return (
+        <div className={styles.home_container}>
+            <div className={styles.home_img1}>
+                <div className={styles.home_desc_big}>상품을 검색하세요</div>
+                <div className={styles.home_search_box1}>
+                    <div className={styles.home_overlay_main}>Search</div>
+                    <div className={styles.home_button1} onClick={goToSecondPage} alt="상세검색으로 이동하는 버튼"/>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+const SecondPage = () => {
+    return (
+        <div className={styles.home_container2}>
+            <div class={styles.home_search_contained}>
+                <p className={styles.home_main_guide}>상품을 검색하세요</p>
+                <div className={styles.home_search_box2} >
+                    <div className={styles.home_img2} alt="웹 페이지 이미지"/>
+                    <div className={styles.home_search_box2_txt}>사과</div>
+                </div>
+            </div>
+            <div>
+            <div className={styles.home_mainguide}>상세 검색을 위해 카테고리를 불러오겠습니다.</div>
+                <div className={styles.home_search_box3}>
+                    <CategoryBoxes/>
+                </div>
+            </div>
+            <div>
+                <a className={styles.home_button2} href="/basket">
+                    <div>다음</div>
+                </a>
+            </div>
+        </div>
     )
+    
+};
+
+
+const detail_category = [
+    {
+        category: "카테고리",
+        car_index: ["원진", "생활/건강", "가구/인테리어", "출산/육아", "디지털/가전"],
+    },
+    {
+        category: "브랜드",
+        car_index: ["늘품", "산지애", "다농이네", "산들네", "과일꾼", "장길영사과"],
+    },
+    {
+        category: "키워드 추천",
+        car_index: ["청송사과", "못난이사과", "세척사과", "꿀사과", "얼음골사과", "문경사과"],
+    },
+    {
+        category: "키워드 추천",
+        car_index: ["청송사과", "못난이사과", "세척사과", "꿀사과", "얼음골사과", "문경사"],
+    },
+    {
+        category: "키워드 추천",
+        car_index: ["청송사과", "못난이사과", "세척사과", "꿀사과", "얼음골사과", "문경사과"],
+    },
+];
+
+function CategoryBoxes() {
+    return (
+        <div className={styles.catebox_body}>
+            {detail_category.map((item, index) => (
+                <div className={styles.catebox_box1} key={item.category}>
+                    <div className={styles.catebox_index1}>{index + 1}. 사과의 '{item.category}'를 추천해주세요(0을 누를 시 생략)</div>
+                    <div className={styles.catebox_index2}>
+                        {item.car_index.map((index) => (
+                            <div className={styles.catebox_index3} key={index}>{index}</div>
+                        ))}
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
 
 export { Home };
