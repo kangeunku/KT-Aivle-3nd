7f2c2c28f4bccb6f77b7b9b3b5e56b153ed05084
eunku <kegoo1997@naver.com>
2023-06-26 11:33:09 +0900

    Model Fix

--- backend/ohsori/models.py
+++ backend/ohsori/models.py
@@ -12,23 +12,31 @@ class CustomToken(models.Model):
     token = models.CharField(max_length=500)
 
 class Goods(models.Model):
-    # goods_no = models.AutoField(help_text="상품고유번호")
-    goods_url = models.URLField (help_text="상품url", unique = True, primary_key=True) 
+    goods_no = models.AutoField(help_text="상품고유번호", primary_key=True)
+    goods_url = models.URLField (help_text="상품url", unique = True) 
     goods_name = models.CharField(max_length=200, help_text='상품이름')
-    goods_info = models.TextField (help_text='상품정보')
+    goods_star = models.TextField(help_text='상품 별점') # goods_views의 get_detail에서 얻어온 정보를 저장 가능한지 확인용
+    goods_price = models.IntegerField(help_text='상품가격')
+    goods_thumb = models.URLField (help_text="상품 대표 이미지")
     reg_date = models.DateTimeField(auto_now_add = True, help_text="등록일자")
     use_yn = models.CharField(max_length=2, help_text="사용여부", null= True)
-    goods_json = models.JSONField(default=dict) # goods_views의 get_detail에서 얻어온 정보를 저장 가능한지 확인용
-    
     
     class Meta:
         db_table = 'ohsori_good'
 
+class Goods_summary(models.Model):
+    goods_no = models.ForeignKey(Goods, on_delete=models.CASCADE, related_name="summary", db_column="goods_no")
+    summary = models.JSONField(default=dict, help_text ="상품 상세 정보 및 이미지 요약")
+    whole_summary = models.TextField(help_text= "상품전체요약")
+    detail = models.JSONField(default=dict, help_text= "상품 배송 정보 및 옵션")
 
+    class Meta:
+        db_table = "goods_summary"
+        
 class Baskets(models.Model):
     basket_no = models.AutoField(help_text="찜하기 고유번호", primary_key = True)
     username = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="baskets",db_column="username") # 정참조 users = Users.objects.get(name='뽀삐') /n  Users_basket = users.basket.all()
-    goods_url = models.ForeignKey(Goods, on_delete=models.CASCADE, related_name="user", db_column="goods_url")                         
+    goods_no = models.ForeignKey(Goods, on_delete=models.CASCADE, related_name="user", db_column="goods_no")                         
     reg_date = models.DateTimeField(auto_now_add = True, help_text="찜한날짜")
     use_yn = models.CharField(max_length=2, help_text="사용여부")
     class Meta:
@@ -43,7 +51,7 @@ class Faq(models.Model):
     
 class Qna(models.Model):
     qna_no = models.AutoField(help_text="문의 번호", primary_key=True)
-    user_no = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="Qna")
+    username = models.ForeignKey(Users, on_delete=models.CASCADE, related_name="Qna")
     question = models.CharField(help_text='개인질문')
     answer = models.TextField (help_text='개인답변', blank = True)
     type = models.CharField(help_text='질문유형')
