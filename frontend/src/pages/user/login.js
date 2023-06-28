bcbac9b136555d5b573f9e6f8e16b0abe0b2aa87
KwanghoKim <home3840@hanmail.net>
2023-06-28 22:07:10 +0900

    main_views.py: sample data로 변경(추후 원복 예정) Home.js: 상세검색 업데이트 Home.module.css: 카테고리 선택 취소 스타일 적용 login.js: 로그인 통신 업데이트(페이지 이동 X)

--- frontend/src/pages/user/login.js
+++ frontend/src/pages/user/login.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 import { Modal } from "../../components";
 import { Send_api } from "../../components";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import {  Home } from "../../pages";
+import {  Header } from "../../components";
+
+import axios from "axios";
 
 const Login = () => {
     const[form, setFrom] = useState({
@@ -8,9 +13,27 @@ const Login = () => {
         "password": "",
     });
 
-    const join_btn = () => {
-        let response_data = Send_api(4, form);
-    }
+    const navigate = useNavigate();
+
+    const join_btn = async () => {
+        const url = "http://127.0.0.1:8000/v1/login/"
+
+        await axios.post(url, form)
+        .then(function (response) {
+            // setResult(JSON.stringify(response.data))
+            // setResult(response.data);
+            console.log(JSON.stringify(response.statusText));
+            const res = JSON.stringify(response.statusText);
+
+            if(res === '"OK"'){
+                navigate();
+            }
+            
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    };
 
     return (
         <>
@@ -35,12 +58,15 @@ const Login = () => {
                     비밀번호 확인
                     <input type="password" className="input_form" value={form.password} onChange={e=> setFrom({...form, password: e.target.value})}/>
                 </label> 
+                <button className="next_step_btn" onClick={() => join_btn()}>로그인</button>
+                {/* <link to="/home" className="next_step_btn">로그인</link> */}
             </div>
-            <button className="next_step_btn" onClick={()=> join_btn()}>로그인</button>
+            
             setId()
             <Modal />
         </>
     );
+    
 }
 
 // export default Input;
