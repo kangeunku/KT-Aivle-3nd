57ddf20a9e542401d4f79ee98fe1618eed0bf589
en7hus1asm <suzie7185@naver.com>
2023-07-03 10:46:27 +0900

    app.js,style.css수정, logosl1234 png추가. 로고에 슬라이드 구성

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,6 @@ import { Routes, Route, Link, useNavigate, useLocation } from "react-router-dom"
 
 import { Home, Basket, EditInfo, Support, Choicelogin, Test } from "./pages";
 import { Header } from "./components";
-import { Logoslide } from "./components/app/logoslide"
 
 
 import { GlobalHotKeys } from 'react-hotkeys';
@@ -16,7 +15,7 @@ const App = () => {
   const [reState, setreState] = useState(false);
 
   const changeislogin = (value) => {
-      setloginState(value);
+    setloginState(value);
   };
 
   const relanding = (value) => {
@@ -24,10 +23,10 @@ const App = () => {
   };
 
   useEffect(() => {
-    if((getCookie("sessionid") != undefined) && (getCookie("usercookieid") != undefined) &&(getCookie("usercookienickname") != undefined)){
+    if ((getCookie("sessionid") != undefined) && (getCookie("usercookieid") != undefined) && (getCookie("usercookienickname") != undefined)) {
       changeislogin(true)
     }
-    else{
+    else {
       changeislogin(false)
     }
   }, [islogin]);
@@ -35,17 +34,17 @@ const App = () => {
   function getCookie(name) {
     let cookieValue = null;
     if (document.cookie && document.cookie !== '') {
-        let cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            let cookie = cookies[i].replace(' ', '');
-             if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
+      let cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].replace(' ', '');
+        if (cookie.substring(0, name.length + 1) === (name + '=')) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
         }
+      }
     }
     return cookieValue;
-}
+  }
 
   const Navigate = useNavigate();
 
@@ -68,7 +67,7 @@ const App = () => {
   const goTest = () => {
     Navigate('/test');
   }
-  
+
   const ReNavigate = () => {
     return (
       <>
@@ -76,16 +75,16 @@ const App = () => {
         <nav className="nav">
           <ul className="nav_lsit">
             <li className={navState === "home" ? "active_list" : null}>
-              <Link to="/home" onClick={() => navState === "home" ?  relanding(true) : setnavState("home") }>검색</Link>
+              <Link to="/home" onClick={() => navState === "home" ? relanding(true) : setnavState("home")}>검색</Link>
             </li>
             <li className={navState === "basket" ? "active_list" : null}>
-              <Link to="/basket" onClick={() => navState === "basket" ? relanding(true) :setnavState("basket")}>찜목록</Link>
+              <Link to="/basket" onClick={() => navState === "basket" ? relanding(true) : setnavState("basket")}>찜목록</Link>
             </li>
             <li className={navState === "editinfo" ? "active_list" : null}>
-              <Link to="/editinfo" onClick={() => navState === "editinfo" ? relanding(true) :setnavState("editinfo")}>회원정보 수정</Link>
+              <Link to="/editinfo" onClick={() => navState === "editinfo" ? relanding(true) : setnavState("editinfo")}>회원정보 수정</Link>
             </li>
             <li className={navState === "support" ? "active_list" : null}>
-              <Link to="/support" onClick={() => navState === "support" ? relanding(true) :setnavState("support")}>고객센터</Link>
+              <Link to="/support" onClick={() => navState === "support" ? relanding(true) : setnavState("support")}>고객센터</Link>
             </li>
             {/* <li className={navState === "test" ? "active_list" : null}>
               <Link to="/test" onClick={() => setnavState("test")}>테스트섹션</Link>
@@ -109,14 +108,14 @@ const App = () => {
     };
 
     const homeClick = () => {
-        console.log('space + q');
-        setnavState("home");
-        goHome();
+      console.log('space + q');
+      setnavState("home");
+      goHome();
     };
     const basketClick = () => {
-        console.log('space + w');
-        setnavState("basket");
-        goBasket();
+      console.log('space + w');
+      setnavState("basket");
+      goBasket();
     };
     const editinfoClick = () => {
       console.log('space + e');
@@ -154,7 +153,7 @@ const App = () => {
             <Modal></Modal>
             <Link to="/home" onClick={() => { setnavState("home"); setloginState(true) }}></Link>
           </h1>
-          <Choicelogin changeislogn={changeislogin}/>
+          <Choicelogin changeislogn={changeislogin} />
         </div>
       </div>
     )
@@ -162,8 +161,8 @@ const App = () => {
   else {
     return (
       <>
-        <Hotkey_global/>
-        
+        <Hotkey_global />
+
         <div className='App'>
           <div className="wrap">
             <div className="side">
@@ -171,7 +170,7 @@ const App = () => {
                 {/* <Link to="/home" onClick={() => {setnavState("home"); setloginState(true)}}></Link> */}
                 <a onClick={() => { setloginState(false) }}>logo</a>
               </h1>
-                <ReNavigate></ReNavigate>
+              <ReNavigate></ReNavigate>
             </div>
             <section className="content">
               <Header changeislogn={changeislogin} />
@@ -180,7 +179,7 @@ const App = () => {
                   {/* 라우터가 적용될 페이지 */}
                   <Route path="/home" element={<Home state={reState} relanding={relanding} />} />
                   <Route path="/basket" element={<Basket state={reState} relanding={relanding} />} />
-                  <Route path="/editinfo" element={<EditInfo state={reState} relanding={relanding} changeislogin={changeislogin}/>} />
+                  <Route path="/editinfo" element={<EditInfo state={reState} relanding={relanding} changeislogin={changeislogin} />} />
                   <Route path="/support" element={<Support state={reState} relanding={relanding} />} />
                   {/* <Route path="/test" element={<Test />} /> */}
                 </Routes>
@@ -211,42 +210,65 @@ const Popup = ({ handleClose }) => {
   const Hotkey_modal = () => {
     // 핫키 설정
     const keyMap_modal = {
-        enter_key: 'enter',
+      enter_key: 'enter',
     };
     const closeClick = () => {
-        console.log('enter');
-        closeWithAnimation();
+      console.log('enter');
+      closeWithAnimation();
     };
     // 핫키 적용 함수
     const handlers_modal = {
-        enter_key: closeClick,
+      enter_key: closeClick,
     };
     return (
-        <>
-            <GlobalHotKeys keyMap={keyMap_modal} handlers={handlers_modal}>
-            </GlobalHotKeys>
-        </>
+      <>
+        <GlobalHotKeys keyMap={keyMap_modal} handlers={handlers_modal}>
+        </GlobalHotKeys>
+      </>
     );
   };
+
+  //로고슬라이드
+  const [activeSlide, setActiveSlide] = useState(0);
+  const slides = [
+    'logosl_1',
+    'logosl_2',
+    'logosl_3',
+    'logosl_4',
+  ];
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveSlide(prevSlide => (prevSlide + 1) % slides.length);
+    }, 3000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <>
-    <Hotkey_modal />
-    <div ref={modalRef} className="intor_graphpop">
-      <div className="content">
-        <h1 className="logo">
-          <a>logo</a>
-        </h1>
-        <p>복잡한 쇼핑 정보를 <br />간단하게 알고 싶다면?</p>
-        <p>ARS를 통한 음성인식 주문 시스템</p>
-        <p>대체텍스트 제공과 요약</p>
-        <p>
-          상품 검색부터 옵션 선택까지 10분이면 가능<br />
-          상품 이미지 대체텍스트 제공과 요약으로 중요한 정보만 쏙!
-        </p>
-        {Logoslide}
-        <button onClick={closeWithAnimation} className="start_closing">Close</button>
+      <Hotkey_modal />
+      <div ref={modalRef} className="intor_graphpop">
+        <div className="content">
+          <h1 className="logo">
+            <a>logo</a>
+          </h1>
+          <p>복잡한 쇼핑 정보를 <br />간단하게 알고 싶다면?</p>
+          <div className="slide-container">
+            {activeSlide === 0 && <h4>ARS를 통해 원하는 상품을 검색하고</h4>}
+            {activeSlide === 1 && <h4>상세 카테고리를 선택하면</h4>}
+            {activeSlide === 2 && <h4>추천상품이나옵니다</h4>}
+            {activeSlide === 3 && <h4>상품이미지 대체텍스트 제공과 요약으로 중요한 정보만!</h4>}
+            <div
+              className={slides[activeSlide]}
+              alt={`Slide ${activeSlide + 1}`}
+            />
+          </div>
+          <button onClick={closeWithAnimation} className="start_closing">시작하기</button>
+        </div>
       </div>
-    </div>
     </>
   );
 };
